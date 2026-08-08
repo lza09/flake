bc90b9313d2f01bdec52d8f36479515324901dc7
lza09 <lza09@proton.me>
2026-08-08 11:34:01 +0900

    update rust.nix 2

--- programs/prismlauncher.nix
+++ programs/prismlauncher.nix
@@ -3,6 +3,33 @@
 {
   environment.systemPackages = with pkgs; [
     (prismlauncher.override {
+      additionalLibs = [
+        libgbm
+        glib
+        nss
+        nspr
+        atk
+        at-spi2-atk
+        libdrm
+        expat
+        libxcb
+        libxkbcommon
+        libX11
+        libXcomposite
+        libXdamage
+        libXext
+        libXfixes
+        libXrandr
+        gtk3
+        pango
+        cairo
+        alsa-lib
+        dbus
+        at-spi2-core
+        cups
+        libxshmfence
+        wayland
+      ];
       jdks = [
         "/opt/zulu-jdk-25"
         "/opt/zulu-jdk-21"
